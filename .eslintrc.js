94d9403c757eede87c37892b64f4297f5a8bb5a2
stevecode <suarezsteve21@gmail.com>
2020-11-20 20:52:32 -0500

    Creating husky rules, eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
+// # "off" or 0 - turn the rule off
+// # "warn" or 1 - turn the rule on as a warning(doesn’t affect exit code)
+// # "error" or 2 - turn the rule on as an error(exit code is 1 when triggered)
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+  },
+  // root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    '@react-native-community',
+  ],
+  rules: {
+    'react/no-did-mount-set-state': 2,
+    'react/no-direct-mutation-state': 2,
+    'react/jsx-uses-vars': 2,
+    'no-undef': 2,
+    semi: 2,
+    'react/prop-types': 2,
+    'react/jsx-no-bind': 2,
+    'react/jsx-no-duplicate-props': 2,
+  },
+  globals: {
+    GLOBAL: false,
+    it: false,
+    expect: false,
+    describe: false,
+  },
 };
